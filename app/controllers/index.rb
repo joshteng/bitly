2e83374ed45db0f710ccdf8a5309305e0b69499d
Apprentice <apprentice@dbc01.local>
2013-04-03 12:26:38 -0700

    add index page and route/controller logic for creating a new link

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,18 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/submiturl' do
+  new_url = Url.create(params[:url])
+  redirect to "/?short_link=#{new_url.short_link}"
+end
+
+get '/all_urls' do
+  Url.all
+end
+
+get '/:short_url' do
+
+# redirect to ''
+
+end
