61094aadd3080b56ee452e1213ae9ebd281424b4
Apprentice <apprentice@dbc01.local>
2013-04-03 17:16:05 -0700

    refactor

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,16 +1,19 @@
+enable :sessions
+
 get '/' do
   # Look in app/views/index.erb
-  @messages = params[:errors]
+  @error = session.delete(:error)
   erb :index
 end
 
 post '/submiturl' do
-  new_url = Url.create(params[:url])
-  if got_errors?(new_url)
+  @url = Url.new(params[:url])
 
-    redirect to "/?errors=#{new_url.errors.messages}"
+  if @url.save
+    erb :index
   else
-    redirect to "/?short_link=#{new_url.short_link}"
+    session[:error] = @url.errors.full_messages.to_sentence
+    redirect '/'
   end
 end
 
@@ -20,13 +23,8 @@ get '/all_urls' do
 end
 
 get '/:short_url' do
-  actual_url = Url.find_by_short_link(params[:short_url])
+  actual_url = Url.find_by_short_link!(params[:short_url])
   actual_url.increment!(:visits)
   # Url.update_counters(actual_url.id, visits: 1 )
   redirect to "#{actual_url.link}"
-end
-
-
-def got_errors?(new_url)
-  !new_url.errors.messages.empty?
 end
