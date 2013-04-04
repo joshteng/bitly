bc5e17c040042309f9f1ea092ea1ccfae1607dc5
Apprentice <apprentice@dbc01.local>
2013-04-03 17:41:10 -0700

    add user model and password simple hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  attr_accessible :email, :password, :password_confirmation
+
+  validate :password_matches
+
+  email_regex =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+                    uniqueness: true,
+                    format: { with: email_regex }
+
+
+
+  def self.hash_password(password)
+    #to make it more sophisticated eventually
+    password + '11111'
+  end
+
+  private
+
+  def password_matches
+    if self.password_confirmation == self.password
+      self.password_hash = User.hash_password(self.password)
+    else
+      errors.add(:password, "does not match")
+    end
+  end
 end
